540da7187b58f2ab732e33e542cf9a70e208bc5c
Andrey Zubov <androiddevaz@mail.ru>
2019-05-10 10:38:23 +0300

    Example with error on InflateFloatingButton

--- app/src/main/java/com/example/androidrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/androidrecyclerview/MainActivity.java
@@ -1,41 +1,124 @@
 package com.example.androidrecyclerview;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ProgressBar mProgressBar;
+    private ListAdapter mListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String[] names = new String[100];
-            for (int i = 0; i < names.length; i++) {
-                names[i] = "Item #" + i;
+        mProgressBar = findViewById(R.id.view_progressbar);
+
+        mRecyclerView = findViewById(R.id.view_list);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        mListAdapter = new ListAdapter(this);
+        mRecyclerView.setAdapter(mListAdapter);
+
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.GONE);
+
+        findViewById(R.id.view_fab).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                Log.d("MainActivity", "Hello FAB!");
+            }
+        });
+
+        new LoadGoogleReposTask().execute();
+    }
+        private List<String> getGoogleReposNames() {
+            final List<String> names = new ArrayList<>();
+
+            final URL url;
+            try {
+                url = new URL("https://api.github.com/users/google/repos");
+            } catch (final MalformedURLException e) {
+                throw new RuntimeException(e);
             }
 
-        final RecyclerView recyclerView = findViewById(R.id.view_list);
-            recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-            recyclerView.setAdapter(new ListAdapter(this, names));
+            final HttpURLConnection urlConnection;
+            final StringBuilder sb = new StringBuilder();
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                final BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String line;
+                do {
+                    line = reader.readLine();
+                    sb.append(line);
+                } while (line != null);
+            } catch (final IOException e) {
+                throw new RuntimeException(e);
+            }
+            urlConnection.disconnect();
+
+            try {
+                final JSONArray reposJsonArray = new JSONArray(sb.toString());
+                for (int i = 0; i < reposJsonArray.length(); i++) {
+                    names.add(reposJsonArray.getJSONObject(i).getString("name"));
+                }
+            } catch (final JSONException e) {
+                throw new RuntimeException(e);
+            }
+            return names;
+        }
+
+    private class LoadGoogleReposTask extends AsyncTask<Void, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(final Void... params) {
+            final List<String> names = getGoogleReposNames();
+            return names.toArray(new String[names.size()]);
+        }
+
+        @Override
+        protected void onPostExecute(final String[] strings) {
+            mProgressBar.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
+
+            mListAdapter.setData(strings);
+            mListAdapter.notifyDataSetChanged();
+        }
     }
 
-    private static class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+        private static class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
         final LayoutInflater mLayoutInflater;
-        private final String[] mData;
+        private String[] mData;
 
-        public ListAdapter(Context context, String[] data) {
-            mLayoutInflater = LayoutInflater.from(context);
-            mData = data;
-        }
+        public ListAdapter(final Context context) {
+            mLayoutInflater = LayoutInflater.from(context);}
+            public void setData( final String[] data) {mData = data;}
 
 
         @Override
