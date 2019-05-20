23f9a0e9b1810b68309f8921083e886e62614de7
Andrey Zubov <androiddevaz@mail.ru>
2019-05-20 20:36:37 +0300

    Different code

--- app/src/main/java/com/example/trymagazine/MainActivity.java
+++ app/src/main/java/com/example/trymagazine/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.trymagazine;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView numberList;
+    private NumbersAdapter numbersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberList = findViewById(R.id.recycler_view1);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        numberList.setLayoutManager(layoutManager);
+
+        numberList.setHasFixedSize(true);
+
+        numbersAdapter = new NumbersAdapter(100);
+        numberList.setAdapter(numbersAdapter);
     }
 }
